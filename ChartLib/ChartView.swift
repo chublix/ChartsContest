005de3b82b46fee7353061b605de0febc1825eee
Andrey <mr_che@ukr.net>
2019-03-12 17:58:36 +0200

    draw experiments

--- ChartLib/ChartView.swift
+++ ChartLib/ChartView.swift
@@ -8,47 +8,66 @@
 
 import UIKit
 
+public class ScrollLayer: CAScrollLayer {
+    
+    public override func display() {
+        super.display()
+        debugPrint("display")
+    }
+    
+}
 
-public class ChartView: UIView {
-
-    private lazy var scrollLayer = CAScrollLayer()
+public class ChartView: UIScrollView {
+    
+    private lazy var contentView = ChartContentView()
+    public var points: [CGPoint] = [] {
+        didSet {
+            contentView.points = points
+        }
+    }
     
     public override init(frame: CGRect) {
         super.init(frame: frame)
-//        scrollLayer.frame = CGRect(origin: .zero, size: CGSize(width: frame.width * 3, height: frame.height))
-//        scrollLayer.scrollMode = .horizontally
-//        contentSize = CGSize(width: frame.width * 3, height: frame.height)
-//        bounces = false
-//        showsHorizontalScrollIndicator = false
-//        showsVerticalScrollIndicator = false
+        let size = CGSize(width: frame.width * 10, height: frame.height)
+        contentView.frame = CGRect(origin: .zero, size: size)
+        contentView.backgroundColor = .lightGray
+        contentSize = size
+        addSubview(contentView)
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-//
-//    private func drawBackground(ctx: CGContext?) {
-//        let step = bounds.height / 5
-//        ctx?.setLineWidth(0.5)
-//        ctx?.setStrokeColor(UIColor.darkGray.cgColor)
-//        for i in 1...4 {
-//            let offset = CGFloat(i) * step
-//            ctx?.strokeLineSegments(between: [CGPoint(x: 0, y: offset), CGPoint(x: bounds.width, y: offset)])
-////            ctx?.move(to:CGPoint(x: 0, y: offset))
-////            ctx?.addLine(to: CGPoint(x: bounds.width, y: offset))
-//        }
-//    }
-//
+    
+}
 
+
+public class ChartContentView: UIView {
+    
+    public var points: [CGPoint] = [] {
+        didSet {
+            self.layoutIfNeeded()
+        }
+    }
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+//        layer.transform = CATransform3DMakeRotation(.pi, 1, 0, 0)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     private func drawBackground(ctx: CGContext?) {
         ctx?.saveGState()
-        let step = bounds.height / 5
-        ctx?.setStrokeColor(UIColor.darkGray.cgColor)
+        let step = bounds.height / 6
+        ctx?.setStrokeColor(UIColor.white.cgColor)
+        ctx?.setLineWidth(0.5)
+        ctx?.setShadow(offset: .zero, blur: 0.0)
         ctx?.beginPath()
-        for i in 1...4 {
+        for i in 1...5 {
             let offset = CGFloat(i) * step
-            ctx?.setLineWidth(0.1)
             ctx?.move(to:CGPoint(x: 0, y: offset))
             ctx?.addLine(to: CGPoint(x: bounds.width, y: offset))
         }
@@ -57,15 +76,70 @@ public class ChartView: UIView {
         ctx?.restoreGState()
     }
     
+    private func drawChart(ctx: CGContext?) {
+        guard points.count > 0 else { return }
+        
+        ctx?.saveGState()
+        ctx?.setStrokeColor(UIColor.blue.cgColor)
+        ctx?.setLineWidth(1)
+        ctx?.beginPath()
+        let preparedPoints = points.map { CGPoint(x: $0.x, y: $0.y) }
+        ctx?.addLines(between: preparedPoints)
+        ctx?.move(to: preparedPoints.last!)
+        ctx?.closePath()
+        ctx?.drawPath(using: .stroke)
+        ctx?.restoreGState()
+    }
+    
+    private func drawCircles(ctx: CGContext?) {
+        guard points.count > 0 else { return }
+        
+        ctx?.saveGState()
+        
+        ctx?.beginPath()
+        ctx?.setFillColor(UIColor.lightGray.cgColor)
+        ctx?.setStrokeColor(UIColor.red.cgColor)
+        let rect = CGRect(x: 0, y: 0, width: 5, height: 5)
+        var neededPoints = points
+        neededPoints.removeFirst()
+        neededPoints.removeLast()
+        neededPoints.forEach { pair in
+            let temp = rect.offsetBy(dx: pair.x - 2.5, dy: pair.y - 2.5)
+            ctx?.addEllipse(in: temp)
+        }
+        ctx?.closePath()
+        
+        ctx?.drawPath(using: .fillStroke)
+        ctx?.restoreGState()
+    }
 
+    private func drawText(ctx: CGContext?) {
+        ctx?.saveGState()
+        
+        let textLayer = CATextLayer()
+        textLayer.frame = CGRect(x: 0, y: 0, width: 90, height: 20)
+        textLayer.font = UIFont.systemFont(ofSize: 14.0)
+        textLayer.fontSize = 14
+        textLayer.foregroundColor = UIColor.green.cgColor
+        textLayer.string = "Test string"
+        ctx?.move(to: CGPoint(x: 50, y: 50))
+        textLayer.render(in: ctx!)
+//        let str = NSAttributedString(string: "Test string", attributes: [.font : UIFont.systemFont(ofSize: 14), .foregroundColor: UIColor.green])
+//        str.draw(at: CGPoint(x: 20, y: 20))
+        
+        ctx?.restoreGState()
+    }
 
     public override func draw(_ rect: CGRect) {
         super.draw(rect)
-        
         let ctx = UIGraphicsGetCurrentContext()
 //        ctx?.clear(rect)
+//        ctx?.setFillColor(UIColor.lightGray.cgColor)
+//        ctx?.fill(rect)
         drawBackground(ctx: ctx)
-//        UIGraphicsEndImageContext()
+        drawChart(ctx: ctx)
+        drawCircles(ctx: ctx)
+        drawText(ctx: ctx)
     }
 
 }
--- TeChartsProject/ViewControllers/ViewController.swift
+++ TeChartsProject/ViewControllers/ViewController.swift
@@ -9,13 +9,26 @@
 import UIKit
 import ChartLib
 
+struct Point: Decodable {
+    let x: CGFloat
+    let y: CGFloat
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let chartView = ChartView(frame: CGRect(x: 15, y: 50, width: 375, height: 280))
-        chartView.backgroundColor = UIColor(hex: 0xfafafa)
+        var points: [CGPoint] = []
+        let url = Bundle.main.url(forResource: "points1", withExtension: "json")
+        if let data = try? Data(contentsOf: url!), let result = try? JSONDecoder().decode([Point].self, from: data) {
+            points = result.map { CGPoint(x: $0.x, y: $0.y) }
+        }
+        
+        let width = view.bounds.width - 20
+        let chartView = ChartView(frame: CGRect(x: 10, y: 50, width: width, height: width * 0.66))
+        chartView.points = points
+        chartView.backgroundColor = UIColor(hex: 0xf1f1f1)
         view.addSubview(chartView)
     }
 
