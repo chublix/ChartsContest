65bcbaabb805c37759b63c16e2fe0b20bde4fff3
Andrey <mr_che@ukr.net>
2019-03-16 21:50:30 +0200

    data mapping

--- TeChartsProject/ViewControllers/ViewController.swift
+++ TeChartsProject/ViewControllers/ViewController.swift
@@ -22,28 +22,39 @@ struct RawChartItem: Decodable {
     let colors: [String: String]
 }
 
-enum Value: Decodable {
-    case string(String)
-    case int(Int)
+//enum Value: Decodable {
+//    case string(String)
+//    case int(Int)
+//    init(from decoder: Decoder) throws {
+//        let container = try decoder.singleValueContainer()
+//        guard let value = ((try? container.decode(String.self)).map(Value.string))
+//            .or((try? container.decode(Int.self)).map(Value.int))
+//            else {
+//                throw DecodingError.typeMismatch(Value.self, DecodingError.Context(codingPath: container.codingPath, debugDescription: "Not a JSON"))
+//        }
+//        self = value
+//    }
+//}
+
+struct Value: Decodable {
+    let string: String?
+    let int: Int?
+    
     init(from decoder: Decoder) throws {
         let container = try decoder.singleValueContainer()
-        guard let value = ((try? container.decode(String.self)).map(Value.string))
-            .or((try? container.decode(Int.self)).map(Value.int))
-            else {
-                throw DecodingError.typeMismatch(Value.self, DecodingError.Context(codingPath: container.codingPath, debugDescription: "Not a JSON"))
-        }
-        self = value
+        self.string = try? container.decode(String.self)
+        self.int = try? container.decode(Int.self)
     }
 }
 
-extension Optional {
-    func or(_ other: Optional) -> Optional {
-        switch self {
-        case .none: return other
-        case .some: return self
-        }
-    }
-}
+//extension Optional {
+//    func or(_ other: Optional) -> Optional {
+//        switch self {
+//        case .none: return other
+//        case .some: return self
+//        }
+//    }
+//}
 
 class ViewController: UIViewController {
 
@@ -56,9 +67,9 @@ class ViewController: UIViewController {
         guard let rawItems = try? JSONDecoder().decode([RawChartItem].self, from: data) else { return [] }
         return rawItems.map { (item) -> ChartItem in
             let columns = item.columns.map { column -> ChartColumn? in
-                guard let id = column.first else { return nil }
-                guard let typeID = item.types[id as String], let type = ChartColumn.ColumnType(rawValue: typeID) else { return nil }
-                let values = column.compactMap { Int($0) }
+                guard let id = column.first?.string else { return nil }
+                guard let typeID = item.types[id], let type = ChartColumn.ColumnType(rawValue: typeID) else { return nil }
+                let values = column.compactMap { $0.int }
                 let color = UIColor(hex: item.colors[id])
                 return ChartColumn(name: item.names[id], values: values, type: type, color: color)
             }
