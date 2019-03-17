c9f4e3d369d0a8666f792c0d9fde48248a4ef239
Andrey <mr_che@ukr.net>
2019-03-17 19:14:52 +0200

    separate layers

---
+++ ChartLib/Sublayers/BackgroundLayer.swift
@@ -0,0 +1,17 @@
+//
+//  BackgroundLayer.swift
+//  ChartLib
+//
+//  Created by Andrey Chuprina on 3/17/19.
+//  Copyright © 2019 Andriy Chuprina. All rights reserved.
+//
+
+import UIKit
+
+internal class BackgroundLayer: CALayer {
+
+    var linesCount: Int = 0
+    var linesColor: UIColor?
+    var linesWidth: CGFloat = 0.0
+    
+}
