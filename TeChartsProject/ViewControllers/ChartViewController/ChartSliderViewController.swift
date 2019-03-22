33128b2788508fc7a8de8f1998bdc053a8865e7e
Elena Chekhova <madlax1993@gmail.com>
2019-03-22 23:55:18 +0200

    slider

--- TeChartsProject/ViewControllers/ChartViewController/ChartSliderViewController.swift
+++ TeChartsProject/ViewControllers/ChartViewController/ChartSliderViewController.swift
@@ -13,12 +13,8 @@ class ChartSliderViewController: UIViewController {
     @IBOutlet private weak var chartView: ChartView!
     @IBOutlet private var thumbView: UIView!
     
-//    private var leftThumbView: UIView!
-//    private var rightThumbView: UIView!
-    
-    private var currentThumbView: UIView?
     private var currentThumbOffset: CGFloat = 0
-    private var position: Position? = nil
+    private var fragment: Fragment? = nil
     
     var value: Float = 0
     
@@ -34,13 +30,7 @@ class ChartSliderViewController: UIViewController {
     
     private func setupThumbs() {
         view.addSubview(thumbView)
-//        let maskLayer = CAShapeLayer()
-//        maskLayer.frame = view.bounds
-//        maskLayer.fillColor = UIColor.yellow.cgColor
-        
         thumbView.frame = CGRect(x: 100, y: 0, width: 80, height: 44)
-//        maskLayer.mask = thumbView.layer
-//        chartView.layer.addSublayer(maskLayer)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -48,36 +38,44 @@ class ChartSliderViewController: UIViewController {
         if let touchView = touch.view, touchView == thumbView {
             let point = touch.location(in: touchView)
             if point.x < 24 {
-                position = .left
+                fragment = .left
             } else if point.x > (thumbView.bounds.width - 24) {
-                position = .right
+                fragment = .right
             } else {
-                position = .center
+                fragment = .center
             }
-            currentThumbOffset = point.x
+            currentThumbOffset = fragment == .right ? thumbView.bounds.width - point.x : point.x
         }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        currentThumbView = nil
         currentThumbOffset = 0
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first, touch.view == thumbView else { return }
-        switch position {
-            case .some(.left): break
-            case .some(.right): break
-            case .some(.center): break
-            case .none: break
-        }
         let point = touch.location(in: view)
-        let x = point.x - currentThumbOffset
-        currentThumbView?.frame.origin.x = x
+        let x = point.x + (fragment != .right ? -currentThumbOffset : currentThumbOffset)
+        let correction = fragment != .right ? thumbView.bounds.width : 0
+        guard x >= 0 && x <= (view.bounds.width - correction) else {
+            return
+        }
+        
+        if fragment == .left {
+            thumbView.frame.size.width += (thumbView.frame.origin.x - x)
+        } else if fragment == .right {
+            let step = x - thumbView.frame.origin.x
+            thumbView.frame.size.width = step
+        }
+        
+        thumbView.frame.size.width = thumbView.frame.size.width < 80 ? 80 : thumbView.frame.size.width
+        
+        if fragment != .right {
+            thumbView.frame.origin.x = x
+        }
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        currentThumbView = nil
         currentThumbOffset = 0
     }
 
@@ -85,7 +83,7 @@ class ChartSliderViewController: UIViewController {
 
 extension ChartSliderViewController {
     
-    enum Position {
+    enum Fragment {
         case left
         case center
         case right
